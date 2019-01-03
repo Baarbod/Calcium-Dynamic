fd83902b90e2b3e7c9f8773679815617dd3d4ab1
Baarbod <bashe001@fiu.edu>
2019-01-03 12:43:56 -0500

    Fixed save file naming. Now the saved parameter set file shows the maximum of each compartment in the file name

--- SA_saveparamset.m
+++ SA_saveparamset.m
@@ -6,18 +6,18 @@ function SA_saveparamset(Data,row)
 
 path = uigetdir(pwd,'Select path for output');
 
-paramSetArrayFiltered = SA_filterparamset(Data);
-% maximum = Data.maximum;
+[paramSetArrayFiltered,ind] = SA_filterparamset(Data);
+maximumFiltered = Data.maximum(ind,:);
 
 
 for i = 1:numel(row)
     irow = row(i);
-%     cMax = num2str(maximum(irow,1));
-%     eMax = num2str(maximum(irow,2));
-%     mMax = num2str(maximum(irow,3));
-%     uMax = num2str(maximum(irow,4));
+    cMax = num2str(maximumFiltered(irow,1));
+    eMax = num2str(maximumFiltered(irow,2));
+    mMax = num2str(maximumFiltered(irow,3));
+    uMax = num2str(maximumFiltered(irow,4));
     P = SA_formatparamset(paramSetArrayFiltered(irow,:));
-%     name = ['pset__Ct_' cMax '_Er_' eMax '_Mt_' mMax '_Md_' uMax '.mat'];
-    name = ['pset__' num2str(i)];
+    name = ['pset__Ct_' cMax '_Er_' eMax '_Mt_' mMax '_Md_' uMax '.mat'];
+%     name = ['pset__' num2str(i)];
     save([path '\' name],'P')
 end
