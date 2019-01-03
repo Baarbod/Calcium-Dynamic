20720dddcdbeca93127c6010c9f43d86fa22bb4f
Baarbod <bashe001@fiu.edu>
2019-01-03 12:40:36 -0500

    Now also returns indicies of the full parameter set array that were filtered. This is useful if information from the full arrays of the other features are wanted for the filtered parameter sets.

--- SA_filterparamset.m
+++ SA_filterparamset.m
@@ -1,4 +1,4 @@
-function paramSetArrayWithOsc = SA_filterparamset(Data)
+function [paramSetArrayWithOsc,ind] = SA_filterparamset(Data)
 
 % Description: function takes a parameter set array generated from the 
 % SA_main script and filters the parameter sets based on desired features.
@@ -7,15 +7,19 @@ paramSetArray = Data.paramSetArray;
 oscillatory = Data.oscillatory;
 
 nParamSet = length(paramSetArray);
+ind = zeros(nParamSet,1);
 
-parfor i = 1:nParamSet
+for i = 1:nParamSet
     oscSum = sum(oscillatory(i,:));
     oscCondition = oscSum == 4;
     
     if ~oscCondition
         paramSetArray(i,:) = 0;
+    else
+        ind(i) = i;
     end
-
+    
 end
 
+ind = ind(any(paramSetArray,2),:);
 paramSetArrayWithOsc = paramSetArray(any(paramSetArray,2),:);
